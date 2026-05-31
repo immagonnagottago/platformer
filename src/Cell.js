bfcb5a40ca94f597ecf7eefe7dddd26d8a3d5dac
immagonnagottago <immagonnagottago@proton.me>
2026-05-31 04:25:50 -0300

    Simplify Cell class constructor and cloning logic
    Refactor Cell class to simplify constructor and clone method.

--- src/Cell.js
+++ src/Cell.js
@@ -1,32 +1,19 @@
+// src/Cell.js
+
 export default class Cell {
+    constructor(type) {
+        this.type = type;
 
-    constructor(opts) {
+        // temperature drives phase changes
+        this.temperature = 20;
 
-        const mass = opts.mass ?? 100;
-        const cohesion = opts.cohesion ?? 50;
-        const energy = opts.energy ?? 0;
-        const conductivity = opts.conductivity ?? 50;
-
-        this.mass = clamp(mass, MIN_MASS, MAX_MASS);
-        this.cohesion = clamp(cohesion, MIN_COHESION, MAX_COHESION);
-        this.energy = clamp(energy, MIN_ENERGY, MAX_ENERGY);
-        this.conductivity = clamp(conductivity, MIN_CONDUCTIVITY, MAX_CONDUCTIVITY);
-
-        // IMPORTANT: material response property (needed for pressure model)
-        this.compressibility = opts.compressibility ?? 1;
-
-        /*
-         * Used by simulation scheduling.
-         */
-        this.lastUpdatedTick = -1;
+        // used for scheduling stability
+        this.updated = false;
     }
 
     clone() {
-        return new Cell({
-            mass: this.mass,
-            cohesion: this.cohesion,
-            energy: this.energy,
-            conductivity: this.conductivity,
-            compressibility: this.compressibility
-        });
+        const c = new Cell(this.type);
+        c.temperature = this.temperature;
+        return c;
     }
+}
